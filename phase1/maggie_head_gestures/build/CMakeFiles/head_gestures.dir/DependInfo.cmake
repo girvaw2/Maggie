c40b0cb8826ec67bda9481305700c9321596d253
Billy <girvaw@gmail.com>
2011-12-17 10:32:33 +0000

    Re-adding gesture functionality...

---
+++ phase1/maggie_head_gestures/build/CMakeFiles/head_gestures.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/billy/ros/Maggie/phase1/maggie_head_gestures/src/head_gestures.cpp" "/home/billy/ros/Maggie/phase1/maggie_head_gestures/build/CMakeFiles/head_gestures.dir/src/head_gestures.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
